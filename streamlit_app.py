c42f80bfc3c2699ff211586f9fbf6aeff130f33e
mhridho1012 <110796631+mhridho1012@users.noreply.github.com>
2024-06-28 11:22:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import requests
 from st_aggrid import AgGrid
+import matplotlib.animation as animation
+from PIL import Image
 
 # Baca dataframe dari file CSV
 house = pd.read_csv('house_clean.csv')
@@ -20,6 +22,29 @@ def main() :
 
 if __name__ == '__main__' :
   main()
+
+ #Fungsi untuk mengupdate plot
+def update_plot(frame, img, implot):
+    implot.set_array(np.rot90(img, frame))
+    return [implot]
+
+# Membaca gambar
+img = Image.open('your_image.png')  # Ganti dengan path ke gambar Anda
+img = np.array(img)
+
+# Membuat figure dan axis
+fig, ax = plt.subplots()
+implot = ax.imshow(img)
+
+# Membuat animasi
+ani = animation.FuncAnimation(fig, update_plot, frames=range(4), fargs=(img, implot), interval=200)
+
+# Menyimpan animasi sebagai file HTML
+ani.save('animation.html', writer='html')
+
+# Membaca file HTML dan menampilkannya di Streamlit
+html_file = open('animation.html', 'r').read()
+st.components.v1.html(html_file, height=300, width=300)
 
 
 
