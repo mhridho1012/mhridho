0734c401a9ee78497d0e89c4f3ec0822502b999c
mhridho1012 <110796631+mhridho1012@users.noreply.github.com>
2024-06-28 11:09:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 import requests
 from st_aggrid import AgGrid
+
 # Baca dataframe dari file CSV
-titanic = pd.read_csv('https://raw.githubusercontent.com/mofdac/-materi-das/main/01.%20Python%20for%20DA/titanic.csv')
+house = pd.read_csv('house_clean.csv')
 
 def main() :
   st.write('Minimal Example')
@@ -13,14 +14,7 @@ def main() :
   st.write('Some Phytagorean Equation : ')
   st.latex('c^2 = a^2+b^2')
   st.write('Contoh dataframe')
-  st.dataframe(titanic)
-  st.write('Contoh JSON')
-  st.json(flight_passanger_api)
-  st.write('Metrics')
-  st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-  st.write('Menampilkan Dataframe dengan St AgGrid')
-  AgGrid(titanic)
-  st.table([x for x in range(1,5)])
+  st.dataframe(house)
 
 if __name__ == '__main__' :
   main()
