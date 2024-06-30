1d28a063577730e5b53a0c0aee5efd5679bf1fa9
mhridho1012 <110796631+mhridho1012@users.noreply.github.com>
2024-07-01 00:04:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,14 @@ house = pd.read_csv('house_clean.csv')
 
 def main() :
   # Judul aplikasi
-st.title('Logistic Regression Model')
+  st.title('Logistic Regression Model')
 
-# Deskripsi atau instruksi
-st.write('Masukkan nilai-nilai untuk melakukan prediksi menggunakan model Logistic Regression.')
+  # Deskripsi atau instruksi
+  st.write('Masukkan nilai-nilai untuk melakukan prediksi menggunakan model Logistic Regression.')
 
-# Contoh input untuk fitur-fitur yang ingin diprediksi
-feature1 = st.number_input('Masukkan nilai fitur 1')
-feature2 = st.number_input('Masukkan nilai fitur 2')
+  # Contoh input untuk fitur-fitur yang ingin diprediksi
+  feature1 = st.number_input('Masukkan nilai fitur 1')
+  feature2 = st.number_input('Masukkan nilai fitur 2')
 
 if __name__ == '__main__' :
   main()
